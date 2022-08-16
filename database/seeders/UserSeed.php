4454100154ea2d80c00a4026df3ed6baec45e354
giovanneluna <gikalunalticg@gmail.com>
2022-08-16 13:53:04 -0300

    Começando a Listar Tweets

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+
+    public function run()
+    {
+        User::factory(10)->create();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Livewire\{
+    ShowTweets
+};
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('tweets', ShowTweets::class);
